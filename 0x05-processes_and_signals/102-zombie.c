85587cb4afb1d9ff16a6580202897551ca312367
Yasminbakr <yasminbakr2019@gmail.com>
2023-09-01 19:15:50 -0700

    d

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,9 +1,38 @@
-#!/usr/bin/env bash
-# well well well
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
 
-file="/tmp/my_process"
+/**
+ * infinite_while - a function that runs forever and returns nothing
+ * Return: 0 in the end
+*/
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
 
-while true; do
-  echo "I am alive!" >> "$file"
-  sleep 2
-done
+/**
+ * main - the entry to a program that creats 5 zombie process
+ * Return: 0 on sucess
+*/
+int main(void)
+{
+	int children_processes = 0;
+	pid_t pid;
+
+	while (children_processes < 5)
+	{
+		pid = fork();
+		if (!pid)
+			break;
+		printf("Zombie process created, PID: %i\n", (int)pid);
+		children_processes++;
+	}
+	if (pid != 0)
+		infinite_while();
+	return (0);
+}
